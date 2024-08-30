f74da321554fbac1acc82397c5a7c663efeddb2a
LAB3-PC18 <lucas0802c@gmail.com>
2024-08-30 11:11:10 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function exibir()
+{
+    alert ('Olá mundo!!!')
+}
+
+function exibir_data_hora()
+{
+    alert (Date())
+}
+
+function exibir_sobre_mim()
+{
+    document.writeln('<h3>Nome:Lucas Silva</h3>')
+    document.writeln('<br>Curso:IPI')
+    document.writeln('<br>Série:2°')
+    document.writeln('<br>Componente curricular:IW II')
+
+}
+
+
